d3ce469b64ee57dcbb311ff2785d521d45efca00
yangyangyang12 <992155966@qq.com>
2016-11-10 09:24:45 +0800

    第一次添加

--- day_13_one_one/src/main/java/com/example/day_13_one_one/MainActivity.java
+++ day_13_one_one/src/main/java/com/example/day_13_one_one/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.day_13_one_one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG="cmd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: 11111111");
     }
 }
